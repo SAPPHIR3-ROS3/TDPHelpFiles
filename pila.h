a8d536140939cbb768f7e5e8ac23ce90a440cc2f
SAPPHIR3ROS3 <47576038+SAPPHIR3-ROS3@users.noreply.github.com>
2022-06-01 00:47:26 +0200

    Create pila.h

---
+++ pila.h
@@ -0,0 +1,19 @@
+/*****************************
+* Tipo astratto pila.        *
+*                            *
+* Realizzazione side-effect. *
+*****************************/
+#pragma once
+
+#include <stdbool.h>
+#include "scl.h"
+
+typedef TipoNodo * Pila;
+
+
+Pila * pilaVuota();                // nuova pila vuota
+Pila * pile_gen(int Length, int min, int max);
+bool estPilaVuota(Pila *c);        // controlla se vuota
+void inPila(Pila *p , T e);        // push; inserisce in testa
+T outPila(Pila* p);                // pop; rimuove dalla testa
+T primoPila(Pila* p);              // valore del primo
