382b1163b99bddc8620e954c65a0cb6599d70ac8
SAPPHIR3ROS3 <47576038+SAPPHIR3-ROS3@users.noreply.github.com>
2022-05-31 23:53:13 +0200

    Create lista.c

---
+++ lista.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lista.h"
+
+
+TipoLista listaVuota() { return NULL; }
+
+int estVuota(TipoLista l) { return (l == NULL); }
+
+TipoLista list_gen(int Length, int min, int max)
+{
+  TipoLista l = listaVuota();
+
+  for (int i = 0; i < Length; i++)
+    l = cons(rand() % (max + 1) + min, l);
+  
+  return l;
+}
+
+TipoLista cons(T e, TipoLista l)
+{
+  TipoLista nuovo = (TipoLista)malloc(sizeof(TipoNodo));
+  nuovo->info = e;
+  nuovo->next = l;
+  return nuovo;
+}
+
+T car(TipoLista l)
+{
+  if (l == NULL)
+  {
+    printf(" ERRORE : lista vuota \n ");
+    exit(1);
+  }
+  return l->info;
+}
+
+TipoLista cdr(TipoLista l)
+{
+  if (l == NULL)
+  {
+    printf(" ERRORE : lista vuota \n ");
+    exit(1);
+  }
+  return l->next;
+}
+
+void printlist_aux(TipoLista l)
+{
+	if (estVuota(l)) return;
+	printf("%d, ", car(l));
+	printlist_aux(cdr(l));
+}
+
+
+void printlist(TipoLista l)
+{
+	printlist_aux(l);
+	printf("\n\n");
+}
