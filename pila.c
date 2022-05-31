bcb482c83d2c922e16686c728d2ae6e64ecb290f
SAPPHIR3ROS3 <47576038+SAPPHIR3-ROS3@users.noreply.github.com>
2022-06-01 00:47:56 +0200

    Create pila.c

---
+++ pila.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "pila.h"
+
+Pila * pilaVuota() {
+	Pila *p = (Pila *)malloc(sizeof(Pila));
+	*p = NULL;
+	return p;
+}
+
+Pila * pile_gen(int Length, int min, int max)
+{
+	Pila * p = pilaVuota();
+
+	for (int i = 0; i < Length; i++)
+		inPila(p, rand() % (max + 1) + min);
+
+	return p;
+}
+
+bool estPilaVuota(Pila *p) {
+	if (!p) {
+		puts("errore: null input");
+		exit(1);
+	}
+  return *p == NULL;
+}
+
+void inPila(Pila *p , T e) {
+	if (!p) {
+		puts("ERRORE: null input");
+		exit(1);
+	}
+	TipoNodo *nuovo = (TipoNodo *)malloc(sizeof(TipoNodo));
+	nuovo->info = e;
+	nuovo->next = *p;
+	*p = nuovo;
+}
+
+T outPila(Pila *p) {
+	if (!p) {
+		puts("ERRORE: null input");
+		exit(1);
+	}
+	if (*p == NULL ){
+		puts("ERRORE: pila vuota");
+		exit(1);
+	}
+	TipoNodo *primo = *p;
+	T primo_valore = primo->info;
+	*p = (*p)->next;
+	free(primo);
+
+	return primo_valore;
+}
+
+T primoPila(Pila* p) {
+	if (!p) {
+		puts("ERRORE: null input");
+		exit(1);
+	}
+	if (*p == NULL ){
+		puts("ERRORE: pila vuota");
+		exit(1);
+	}
+  return (*p)->info;
+}
